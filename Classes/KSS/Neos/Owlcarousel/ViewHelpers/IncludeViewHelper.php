edf3dd08225731f341d4a429ea08aca598d33649
Sönke Michelsen <s.michelsen@axovis.com>
2015-12-15 16:15:06 +0100

    [TASK] carousel update to 2.0

--- Classes/KSS/Neos/Owlcarousel/ViewHelpers/IncludeViewHelper.php
+++ Classes/KSS/Neos/Owlcarousel/ViewHelpers/IncludeViewHelper.php
@@ -1,17 +1,43 @@
 <?php
 namespace KSS\Neos\Owlcarousel\ViewHelpers;
 
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Resource\ResourceManager;
 use TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 class IncludeViewHelper extends AbstractViewHelper {
     /**
-     * @param boolean $minified
-     * @return string
+     * @Flow\Inject
+     * @var ResourceManager
      */
-    public function render($minified = true) {
-        return '
-            <link rel="stylesheet" type="text/css" href="resource://KSS.Neos.Owlcarousel/Public/Styles/owl.carousel.css" />
-            <script type="text/javascript" src="resource://KSS.Neos.Owlcarousel/Public/JavaScript/owl.carousel' . ($minified ? '.min' : '') . '.js"></script>
-        ';
+    protected $resourceManager;
+
+    /**
+     * @var boolean
+     */
+    protected $escapeOutput = false;
+
+    /**
+     * @param boolean $minified
+     * @param string $include
+     * @return string
+     * @throws \Exception
+     */
+    public function render($minified = true,$include = 'all') {
+        if(!in_array($include,array('all','css','js'))) {
+            throw new \Exception('invalid include parameter. valid values are "all", "css" and "js".');
+        }
+
+        $return = '';
+        if($include == 'css' || $include == 'all') {
+            $css = $this->resourceManager->getPublicPackageResourceUri('KSS.Neos.Owlcarousel', 'Styles/owl.carousel.css');
+            $return .= '<link rel="stylesheet" type="text/css" href="' . $css . '" />';
+        }
+        if($include == 'js' || $include == 'all') {
+            $js = $this->resourceManager->getPublicPackageResourceUri('KSS.Neos.Owlcarousel', 'JavaScript/owl.carousel' . ($minified ? '.min' : '') . '.js');
+            $return .= '<script type="text/javascript" src="' . $js . '"></script>';
+        }
+
+        return $return;
     }
 }
